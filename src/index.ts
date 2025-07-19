de81860a6f84480533dbfee76c54ae30afe1756d
ericdeveloper <ericspsantos@gmail.com>
2025-07-19 19:04:41 -0300

    iniciando projeto

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // src/index.ts
 import express from 'express';
 import cors from 'cors';
-import router from './routes';      // ajuste se sua pasta for diferente
-
+import router from './routes';      
+import userRouter from './routes/user';
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/', router);
+app.use('/', userRouter);
 
 export default app;
