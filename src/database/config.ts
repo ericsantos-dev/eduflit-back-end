cfe3033934688e01112eb9577ee9b0b6499a431a
ericdeveloper <ericspsantos@gmail.com>
2025-07-19 18:58:27 -0300

    iniciando projeto

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,4 +1,4 @@
-// src/data-source.ts
+import 'reflect-metadata';               
 import { DataSource } from 'typeorm';
 import { User } from '../models/User';
 if (process.env.NODE_ENV !== 'production') {
