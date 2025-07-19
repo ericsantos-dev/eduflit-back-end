ac881dfaa7bab559840e55b0a71fe265d5bda962
ericdeveloper <ericspsantos@gmail.com>
2025-07-19 11:36:13 -0300

    iniciando projeto

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import UserService from '../services/userService';
+import UserService from '../services/UserService';
 
 const userRouter = Router();
 
