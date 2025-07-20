ea41443d1793cd35c05d720d7c4c30814db0ae40
ericdeveloper <ericspsantos@gmail.com>
2025-07-20 10:37:59 -0300

    iniciando projeto

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,8 @@ import router from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 app.use('/', router);
-import { Router, Request, Response, NextFunction } from 'express';
-import UserService from './services/UserService';
-
-
-// Get all users
-app.get('/users', async (_req: Request, res: Response, next: NextFunction) => {
-  try {
-    const users = await UserService.fetchAllUsers();
-    res.json(users);
-  } catch (error) {
-    next(error);
-  }
-});
-
 
 export default app;
