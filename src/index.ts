3ba4de88f974ffe98e6a4faeaddd95d7c83dac3e
ericdeveloper <ericspsantos@gmail.com>
2025-07-20 09:51:42 -0300

    iniciando projeto

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,10 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes';      
-
+import userRouter from './routes/user';
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/', router);
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+app.use('/', userRouter);
 
 export default app;
