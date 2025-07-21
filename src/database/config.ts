1a4e9c53a6afbd4c85108f0f3cefc88d3e2f028c
ericdeveloper <ericspsantos@gmail.com>
2025-07-21 11:05:08 -0300

    iniciando projeto

--- src/database/config.ts
+++ src/database/config.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const sequelize = new Sequelize({
-  host: process.env.PG_HOST,
+  host: "aws-0-us-east-2.pooler.supabase.com",
   port: parseInt(process.env.PG_PORT || '5432'),
-  username: process.env.PG_USUARIO,
-  password: process.env.PG_SENHA,
-  database: process.env.PG_BANCO,
+  username: "postgres.rvjqqeojodwjsqeeodsk",
+  password: "eric550spfc",
+  database: "postgres",
   dialect: 'postgres',  // <--- aqui
   logging: console.log,
 });
