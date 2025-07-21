6dd7d7591e11e4e9523b23e1fdb96f8e362d3fa5
ericdeveloper <ericspsantos@gmail.com>
2025-07-21 09:29:45 -0300

    iniciando projeto

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,3 @@
-
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).send('Olá! Este é um texto simples do backend na Vercel.');
+export default function handler(req, res) {
+  res.status(200).json({ message: "Olá do back-end serverless da Vercel!" });
 }
